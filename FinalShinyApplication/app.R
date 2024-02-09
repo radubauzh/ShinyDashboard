2a4d9bd0f0e922f1e77ae2f29012bdbb401cdd24
radaubauzh <rafael.dubach@uzh.ch>
2024-02-09 14:00:20 +0100

    update r

--- FinalShinyApplication/app.R
+++ FinalShinyApplication/app.R
@@ -1,14 +1,30 @@
+# README: You will find the Package here: https://github.com/radubauzh
+# I will take it down / make it private later
+
+# Install remotes packages
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+
+# Install MyFinalPackage from GitHub
+remotes::install_github("radubauzh/ChurnProbability-r-package")
+
 library(shiny)
 library(leaflet)
 library(data.table)
+
+# IMPORTANT: 
+# Before deploying to Shiny, ensure any locally built packages are removed.
+# Reason: Deployment may fail if a locally built package hasn't been published. Shiny won't find the package locally.
+# Solution: Always install packages from your GitHub repository to ensure successful deployment.
+
 library(MyFinalPackage)
 
 # Load and merge data
-customers <- fread("data_customer.csv")
+customers <- fread("data_customer.csv") # could also load from github
 personal <- fread("data_personal.csv")
 merged_data <- merge(customers, personal, by = "CustomerId")
 
-# Assuming Exited and Gender were meant to be used in analysis/modeling previously
 merged_data$Exited <- as.factor(merged_data$Exited)
 merged_data$Gender <- as.factor(merged_data$Gender)
 
@@ -54,3 +70,7 @@ server <- function(input, output) {
 }
 
 shinyApp(ui = ui, server = server)
+
+
+# setwd("/Users/rafaeldubach/ShinyDashboard/FinalShinyApplication")
+# rsconnect::deployApp()
